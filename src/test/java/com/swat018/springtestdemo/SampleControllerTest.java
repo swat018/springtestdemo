2acbf4c7ffa32f2bb4b57eb32863c2b94680f825
jinwoo park <swat018@gmail.com>
2020-05-28 23:25:27 +0900

    테스트 - OutputCapture

--- src/test/java/com/swat018/springtestdemo/SampleControllerTest.java
+++ src/test/java/com/swat018/springtestdemo/SampleControllerTest.java
@@ -1,5 +1,7 @@
 package com.swat018.springtestdemo;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -16,7 +19,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -25,6 +30,9 @@ import static org.mockito.Mockito.when;
 //@AutoConfigureMockMvc
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
+
+    @Rule
+    public OutputCaptureRule  outputCapture = new OutputCaptureRule();
 
     @Autowired
     MockMvc mockMvc;
@@ -50,5 +58,9 @@ public class SampleControllerTest {
  /*       webTestClient.get().uri("/hello").exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class).isEqualTo("hello jinwoo");*/
+        assertThat(outputCapture.toString())
+                .contains("holoman")
+                .contains("skip");
+
     }
 }
