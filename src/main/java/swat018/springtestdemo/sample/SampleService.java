51c6124b543c75e02f9c9d9ca6971e98151a6b0f
jinwoo park <swat018@gmail.com>
2021-08-02 09:45:31 +0900

    테스트

---
+++ src/main/java/swat018/springtestdemo/sample/SampleService.java
@@ -0,0 +1,11 @@
+package swat018.springtestdemo.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "jinwoo";
+    }
+}
